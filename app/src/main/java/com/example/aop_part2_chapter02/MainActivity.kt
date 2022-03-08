e94771014af0536a5399ce5a799e82f33637fec9
gaeun <geun5744@gmail.com>
2022-03-08 18:01:20 +0900

    Final UI and Function

--- app/src/main/java/com/example/aop_part2_chapter02/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -51,13 +52,24 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue=45
 
         initRunButton()
+        initAddButton()
+        initClearButton()
     }
 
     private fun initRunButton(){
         runButton.setOnClickListener {
-            val list=getRandomNmber()
+            val list=getRandomNumber()
 
-            Log.d("MainActivity",list.toString())
+            didRun=true
+
+            list.forEachIndexed { index, number ->
+                val textView=numberTextViewList[index]
+
+                textView.text=number.toString()
+                textView.isVisible=true
+
+                setNumberBackground(number,textView)
+            }
         }
     }
 
@@ -81,23 +93,51 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible=true
             textView.text=numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value,textView)
+
             pickNumberSet.add(numberPicker.value)
 
         }
     }
 
+    private fun setNumberBackground(number:Int,textView: TextView){
 
-    private fun getRandomNmber():List<Int>{
+        when(number){
+            in 1..10 -> textView.background=ContextCompat.getDrawable(this,R.drawable.circle_yellow)
+            in 11..20 -> textView.background=ContextCompat.getDrawable(this,R.drawable.circle_blue)
+            in 21..30 -> textView.background=ContextCompat.getDrawable(this,R.drawable.circle_red)
+            in 31..40 -> textView.background=ContextCompat.getDrawable(this,R.drawable.circle_gray)
+            else -> textView.background=ContextCompat.getDrawable(this,R.drawable.circle_green)
+        }
+    }
+
+    private fun initClearButton(){
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible=false
+            }
+            didRun=false
+        }
+    }
+
+
+    private fun getRandomNumber():List<Int>{
 
         val numberList= mutableListOf<Int>().apply {
             for(i in 1..45){
+
+                if(pickNumberSet.contains(i)){
+                    continue
+                }
+
                 this.add(i)
             }
         }
 
         numberList.shuffle()
 
-        val newList=numberList.subList(0,6)
+        val newList=pickNumberSet.toList()+numberList.subList(0,6-pickNumberSet.size)
 
         return newList.sorted()
 
